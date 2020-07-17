caaaadf4729d15c24ba38637fc025f32f4f0320f
D3lph1 <d3lph1.contact@gmail.com>
2020-07-17 16:39:16 +0400

    Removed jdbc session

--- src/main/java/ru/ssau/practice/config/AppConfig.java
+++ src/main/java/ru/ssau/practice/config/AppConfig.java
@@ -3,12 +3,10 @@ package ru.ssau.practice.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 import ru.ssau.practice.service.db.pagination.JpaQueryPaginator;
 import ru.ssau.practice.service.db.pagination.Paginator;
 
 @Configuration
-@EnableJdbcHttpSession
 public class AppConfig
 {
     @Bean
